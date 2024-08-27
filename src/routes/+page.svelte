150eaefc30ad5214717d8d4ab8500cc883b36006
Cameron Kruse <14115927+cameronkruse@users.noreply.github.com>
2024-08-27 10:57:27 -0700

    udpating homepage

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,53 +1,40 @@
 <script>
 	import leaves from '$lib/images/leaf-texture.png';
+	import leaves2 from '$lib/images/leaf-texture2.png';
 </script>
 
 <svelte:head>
 	<title>Home</title>
 	<meta name="description" content="CI AI Experiments" />
 </svelte:head>
-<div class="card bg-base-100 image-full w-96 shadow-xl">
-	<figure>
-		<img
-			src={leaves}
-			alt="leaves"
-		/>
-	</figure>
-	<div class="card-body">
-		<h2 class="card-title">Research Agent Demo</h2>
-		<p>Chat with a research agent to learn more about climate change and it's impacts.</p>
-		<div class="card-actions justify-end">
-			<button class="btn btn-primary" href="./agent-chat">Explore</button>
+<!-- two columns -->
+<div class="columns-2">
+	<div class="card bg-base-100 image-full w-96 shadow-xl">
+		<figure>
+			<img src={leaves} alt="leaves" />
+		</figure>
+		<div class="card-body">
+			<h2 class="card-title">Research Agent Demo</h2>
+			<p>Chat with a research agent to learn more about climate change and it's impacts.</p>
+			<div class="card-actions justify-end">
+				<button class="btn btn-primary" onclick="window.location.href='./map-chat'"
+					>Explore</button
+				>
+			</div>
 		</div>
 	</div>
-</div>
-<div class="card lg:card-side bg-base-100 shadow-xl">
-	<figure>
-		<img
-			src="https://img.daisyui.com/images/stock/photo-1494232410401-ad00d5433cfa.webp"
-			alt="Album"
-		/>
-	</figure>
-	<div class="card-body">
-		<h2 class="card-title">Research Agent Demo</h2>
-		<p>Chat with a research agent to learn more about climate change and it's impacts.</p>
-		<div class="card-actions justify-end">
-			<button class="btn btn-primary">Explore</button>
-		</div>
-	</div>
-</div>
-<div class="card lg:card-side bg-base-100 shadow-xl">
-	<figure>
-		<img
-			src="https://img.daisyui.com/images/stock/photo-1494232410401-ad00d5433cfa.webp"
-			alt="Album"
-		/>
-	</figure>
-	<div class="card-body">
-		<h2 class="card-title">Map Explorer</h2>
-		<p>Explore the impacts of deforestation on warming temperatures</p>
-		<div class="card-actions justify-end">
-			<button class="btn btn-primary">Explore</button>
+	<div class="card bg-base-100 image-full w-96 shadow-xl">
+		<figure>
+			<img src={leaves2} alt="leaves" />
+		</figure>
+		<div class="card-body">
+			<h2 class="card-title">Map Explorer</h2>
+			<p>Explore the impacts of deforestation on warming temperatures</p>
+			<div class="card-actions justify-end">
+				<button class="btn btn-primary" onclick="window.location.href='./agent-chat'"
+					>Explore</button
+				>
+			</div>
 		</div>
 	</div>
 </div>
