d683a85448139c59374dac7a9ac5e1576b50ed67
Cameron Kruse <14115927+cameronkruse@users.noreply.github.com>
2024-10-18 11:41:25 -0700

    adding data and updating map

--- src/routes/map-chat/+page.svelte
+++ src/routes/map-chat/+page.svelte
@@ -1,46 +1,12 @@
 <script>
-	import AgentChat from '$lib/components/AgentChat.svelte';
-	import Sidebar from '$lib/components/AssistantSidebar.svelte';
-	import Dashboard from '$lib/components/Dashboard.svelte';
-	import DeforestationStats from '$lib/components/DeforestationStats.svelte';
-	import Map from '$lib/components/Map.svelte';
-	import MapAgent from '$lib/components/MapAgent.svelte';
-	import { Coords } from '$lib/stores.js';
 	import { onMount } from 'svelte';
 
-	let isMobile = false;
-
-	function checkMobile() {
-		isMobile = window.innerWidth < 768; // Adjust this breakpoint as needed
-	}
-
-	onMount(() => {
-		checkMobile();
-		window.addEventListener('resize', checkMobile);
-		return () => window.removeEventListener('resize', checkMobile);
-	});
 </script>
 
-<main class="flex-1">
-	<div class="h-screen flex flex-col">
-		<div class="card shadow-xl h-[15vh] w-full overflow-y-auto p-0">
-		  <div class="card-body m-1 p-0">
-			<DeforestationStats />
-		  </div>
-		</div>
-	  
-		<div class="flex-1 flex {isMobile ? 'flex-col' : 'flex-row'} overflow-hidden">
-		  <div class="card shadow-xl {isMobile ? 'h-[42.5vh]' : 'w-1/2'} overflow-y-auto m-1">
-			<div class="card-body p-0">
-				<Map />
-			</div>
-		  </div>
-	  
-		  <div class="card  shadow-xl {isMobile ? 'h-[42.5vh]' : 'w-1/2'} overflow-y-auto m-1">
-			<div class="card-body p-2">
-				<MapAgent />
-			</div>
-		  </div>
-		</div>
-	  </div>
+<main class="flex-1 flex items-center justify-center" style="height: 100vh;">
+	<h1 class="text-4xl font-bold">AI Map Explorer is coming soon!</h1>
 </main>
+
+<style>
+	
+</style>
